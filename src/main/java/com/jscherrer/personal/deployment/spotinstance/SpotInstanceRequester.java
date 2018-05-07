735e386b56f16cb788aaca6d5b7449095ebd3333
Jonathan <jonathan.scherrer@gmail.com>
2018-05-07 13:10:19 +0100

    Assign first keypairname to requests

--- src/main/java/com/jscherrer/personal/deployment/spotinstance/SpotInstanceRequester.java
+++ src/main/java/com/jscherrer/personal/deployment/spotinstance/SpotInstanceRequester.java
@@ -123,6 +123,20 @@ public class SpotInstanceRequester {
         ArrayList<String> securityGroups = new ArrayList<>();
         securityGroups.add(AWSConstants.SECURITY_GROUP_NAME);
         launchSpecification.setSecurityGroups(securityGroups);
+        launchSpecification.setKeyName(getKeyPairName());
         return launchSpecification;
+    }
+
+    private String getKeyPairName() {
+        DescribeKeyPairsRequest keyPairsRequest = new DescribeKeyPairsRequest();
+        DescribeKeyPairsResult describeKeyPairsResult = EC2.describeKeyPairs(keyPairsRequest);
+
+        describeKeyPairsResult.getKeyPairs();
+        if (describeKeyPairsResult.getKeyPairs().size() == 1) {
+            return describeKeyPairsResult.getKeyPairs().get(0).getKeyName();
+        } else {
+            LOG.warn("Multiple KeyPairNames exist, not authing any");
+            return null;
+        }
     }
 }
